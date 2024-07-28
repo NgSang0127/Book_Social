aa1c8ee014c972d0a8a5a91e2145887408a60a00
ITITIU20292 <ITITIU20292@student.hcmiu.edu.vn>
2024-07-28 18:36:27 +0700

    Handling exception

---
+++ src/main/java/org/sang/booksocialnetwork/handler/ExceptionResponse.java
@@ -0,0 +1,27 @@
+package org.sang.booksocialnetwork.handler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.util.Map;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(Include.NON_EMPTY)//các trường rỗng (null,0,"") sẽ duoc bỏ qua khi chuyển về json
+public class ExceptionResponse {
+
+	private Integer businessErrorCode;
+	private String businessErrorDescription;
+	private String error;
+	private Set<String> validationErrors;
+	private Map<String,String> errors;
+
+}
